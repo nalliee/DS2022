b8e7bda484710d8770d742be7a812bfb8e9371f6
Natasha Lie 李沁霞 <108567215+nalliee@users.noreply.github.com>
2022-11-08 17:02:46 +0700

    create helloworld

---
+++ helloworld/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/**
+ * using namespace std to group entities like functions under a name.
+ * 
+ */
+using namespace std;
+
+int main() {
+
+    /**
+     * cout to compile out the Hello World string.
+     * endl to end the line.
+     */
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
